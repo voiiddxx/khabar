fb9c421215e5052025556c218eaef92176f01dcf
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-06-27 18:11:07 +0530

    packages and some other stuff

---
+++ lib/utils/keyfile.dart
@@ -0,0 +1,4 @@
+class apikey {
+  static String key =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=9212799a07e54d75acb5496798ea4f49";
+}
---
+++ lib/utils/renderimage.dart
@@ -0,0 +1,4 @@
+class RenderImages {
+  static String imageurl =
+      "https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80";
+}
